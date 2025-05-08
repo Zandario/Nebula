46ed0b03883bfdd04bc200a698a7d3c7823b1b20
Zandario <zandarioh@gmail.com>
2025-05-07 22:26:01 -0400

    NanoTemplateClass

--- nano/js/nano_template.js
+++ nano/js/nano_template.js
@@ -1,116 +1,119 @@
+/**
+ * @fileOverview
+ * This file contains the NanoTemplate class, which is responsible for loading, compiling, and rendering templates using doT.js.
+ * It also manages template data and helpers, allowing for dynamic content generation in a browser context.
+ */
+class NanoTemplateClass {
+	constructor() {
+		this._templateData = {};
+		this._templateFileName = '';
+		this._templates = {};
+		this._compiledTemplates = {};
+		this._helpers = {};
+	}
 
-var NanoTemplate = function () {
+	init() {
+		const templateDataElement = document.querySelector('#TemplateData');
+		const initialDataElement = document.querySelector('#InitialData');
 
-	var _templateData = {};
-	var _templateFileName = '';
+		if (templateDataElement && initialDataElement) {
+			this._templateData = JSON.parse(templateDataElement.textContent);
+			const initialData = JSON.parse(initialDataElement.textContent);
+			this._templateFileName = initialData['config']['templateFileName'];
 
-	var _templates = {};
-	var _compiledTemplates = {};
+			if (!this._templateData) {
+				alert('Error: Template data did not load correctly.');
+			}
 
-	var _helpers = {};
-
-	var init = function () {
-		// We store TemplateData in the script tag
-		_templateData = JSON.parse(document.querySelector('#TemplateData').textContent);
-		let initialData = JSON.parse(document.querySelector('#InitialData').textContent);
-		_templateFileName = initialData['config']['templateFileName'];
-
-		if (_templateData == null)
-		{
-			alert('Error: Template data did not load correctly.');
+			this.loadAllTemplates();
+		} else {
+			alert('Error: Required elements (#TemplateData or #InitialData) not found in the DOM.');
 		}
+	}
 
-		loadAllTemplates();
-	};
+	async loadAllTemplates() {
+		try {
+			const response = await fetch(this._templateFileName, { cache: 'no-store' });
+			if (!response.ok) {
+				throw new Error('Failed to fetch templates file.');
+			}
 
-	var loadAllTemplates = function () {
-		$.when($.ajax({
-			url: _templateFileName,
-			cache: false,
-			dataType: 'json'
-		}))
-		.done(function(allTemplates) {
+			const allTemplates = await response.json();
 
-			for (var key in _templateData)
-			{
-				var templateMarkup = allTemplates[_templateData[key]];
+			for (const key in this._templateData) {
+				let templateMarkup = allTemplates[this._templateData[key]];
 				templateMarkup += '<div class="clearBoth"></div>';
 
-				try
-				{
-					NanoTemplate.addTemplate(key, templateMarkup);
-				}
-				catch(error)
-				{
-					alert('ERROR: Loading template ' + key + '(' + _templateData[key] + ') failed with error: ' + error.message);
+				try {
+					this.addTemplate(key, templateMarkup);
+				} catch (error) {
+					alert(`ERROR: Loading template ${key} (${this._templateData[key]}) failed with error: ${error.message}`);
 					return;
 				}
-				delete _templateData[key];
+				delete this._templateData[key];
 			}
-			$(document).trigger('templatesLoaded');
-		})
-		.fail(function () {
+
+			const event = new Event('templatesLoaded');
+			document.dispatchEvent(event);
+		} catch (error) {
 			alert('ERROR: Failed to locate or parse templates file.');
-		});
-	};
-
-	var compileTemplates = function () {
-
-		for (var key in _templates) {
-			try {
-				_compiledTemplates[key] = doT.template(_templates[key], null, _templates)
-			}
-			catch (error) {
-				alert('ERROR: Compiling template key "' + key + '" ("' + _templateData[key] + '") failed with error: ' + error);
-			}
 		}
-	};
+	}
 
-	return {
-		init: function () {
-			init();
-		},
-		addTemplate: function (key, templateString) {
-			_templates[key] = templateString;
-		},
-		templateExists: function (key) {
-			return _templates.hasOwnProperty(key);
-		},
-		parse: function (templateKey, data) {
-			if (!_compiledTemplates.hasOwnProperty(templateKey) || !_compiledTemplates[templateKey]) {
-				if (!_templates.hasOwnProperty(templateKey)) {
-					alert('ERROR: Template "' + templateKey + '" does not exist in _compiledTemplates!');
-					return '<h2>Template error (does not exist)</h2>';
-				}
-				compileTemplates();
-			}
-			if (typeof _compiledTemplates[templateKey] != 'function') {
-				return '<h2>Template error (failed to compile)</h2>';
-			}
-			return _compiledTemplates[templateKey].call(this, data['data'], data['config'], _helpers);
-		},
-		addHelper: function (helperName, helperFunction) {
-			if (!jQuery.isFunction(helperFunction)) {
-				alert('NanoTemplate.addHelper failed to add ' + helperName + ' as it is not a function.');
-				return;
-			}
-
-			_helpers[helperName] = helperFunction;
-		},
-		addHelpers: function (helpers) {
-			for (var helperName in helpers) {
-				if (!helpers.hasOwnProperty(helperName))
-				{
-					continue;
-				}
-				NanoTemplate.addHelper(helperName, helpers[helperName]);
-			}
-		},
-		removeHelper: function (helperName) {
-			if (helpers.hasOwnProperty(helperName))
-			{
-				delete _helpers[helperName];
+	compileTemplates() {
+		for (const key in this._templates) {
+			try {
+				this._compiledTemplates[key] = doT.template(this._templates[key], null, this._templates);
+			} catch (error) {
+				alert(`ERROR: Compiling template key "${key}" failed with error: ${error}`);
 			}
 		}
 	}
-}();
+
+	addTemplate(key, templateString) {
+		this._templates[key] = templateString;
+	}
+
+	templateExists(key) {
+		return Object.prototype.hasOwnProperty.call(this._templates, key);
+	}
+
+	parse(templateKey, data) {
+		if (!Object.prototype.hasOwnProperty.call(this._compiledTemplates, templateKey) || !this._compiledTemplates[templateKey]) {
+			if (!Object.prototype.hasOwnProperty.call(this._templates, templateKey)) {
+				alert(`ERROR: Template "${templateKey}" does not exist in _compiledTemplates!`);
+				return '<h2>Template error (does not exist)</h2>';
+			}
+			this.compileTemplates();
+		}
+		if (typeof this._compiledTemplates[templateKey] !== 'function') {
+			return '<h2>Template error (failed to compile)</h2>';
+		}
+		return this._compiledTemplates[templateKey].call(this, data['data'], data['config'], this._helpers);
+	}
+
+	addHelper(helperName, helperFunction) {
+		if (typeof helperFunction !== 'function') {
+			alert(`NanoTemplate.addHelper failed to add ${helperName} as it is not a function.`);
+			return;
+		}
+
+		this._helpers[helperName] = helperFunction;
+	}
+
+	addHelpers(helpers) {
+		for (const helperName in helpers) {
+			if (Object.prototype.hasOwnProperty.call(helpers, helperName)) {
+				this.addHelper(helperName, helpers[helperName]);
+			}
+		}
+	}
+
+	removeHelper(helperName) {
+		if (Object.prototype.hasOwnProperty.call(this._helpers, helperName)) {
+			delete this._helpers[helperName];
+		}
+	}
+}
+
+const NanoTemplate = new NanoTemplateClass();
